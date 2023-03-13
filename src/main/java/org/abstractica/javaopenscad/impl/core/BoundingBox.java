dc69f69a9e4b57b6d2e02e8d4b569a81c8187200
tgrundtvig <TGrundtvig@gmail.com>
2023-03-13 15:06:02 +0100

    bugfix

--- src/main/java/org/abstractica/javaopenscad/impl/core/BoundingBox.java
+++ src/main/java/org/abstractica/javaopenscad/impl/core/BoundingBox.java
@@ -3,19 +3,19 @@ package org.abstractica.javaopenscad.impl.core;
 public class BoundingBox
 {
 	public final double xMin;
-	public final double xMax;
 	public final double yMin;
-	public final double yMax;
 	public final double zMin;
+	public final double xMax;
+	public final double yMax;
 	public final double zMax;
 
-	public BoundingBox(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax)
+	public BoundingBox(double xMin, double yMin, double zMin, double xMax, double yMax, double zMax)
 	{
 		this.xMin = xMin;
-		this.xMax = xMax;
 		this.yMin = yMin;
-		this.yMax = yMax;
 		this.zMin = zMin;
+		this.xMax = xMax;
+		this.yMax = yMax;
 		this.zMax = zMax;
 	}
 }
